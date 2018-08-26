5545b10104d20ee1ebd9639ea296d295374f2187
Manmeet Tarun <meetutarun16@gmail.com>
2018-08-26 11:26:00 +0530

    Push

--- config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    db_url: 'mongodb://manmeet:man1meet@ds020208.mlab.com:20208/paypalhack',
-    enableCors: true
-}
--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    "meetupKey": '3c5b16446a5b3d7a1f3c585b2cf6b29'
-}
